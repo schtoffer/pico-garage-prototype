848052f0aa7b5edb07bbea7572950b51e6771414
Christoffer Andersen <christofferandersen@Christoffers-MacBook-Air.local>
2023-05-14 10:27:01 +0200

    bringing on the main.py from Pico to repository

--- main.py
+++ main.py
@@ -11,20 +11,9 @@ import utime as time
 import uasyncio as asyncio
 from machine import Pin
 import ujson as json
-from board import Board
 
-BOARD_TYPE = Board().type
-print("Board type: " + BOARD_TYPE)
-
-if BOARD_TYPE == Board.BoardType.PICO_W:
-    led = Pin("LED", Pin.OUT)
-elif BOARD_TYPE == Board.BoardType.PICO:
-    led = Pin(25, Pin.OUT)
-elif BOARD_TYPE == Board.BoardType.ESP8266:
-    led = Pin(2, Pin.OUT)
-else:
-    led = Pin(2, Pin.OUT)
-
+led = Pin("LED", Pin.OUT)
+led.off()
 
 blink_off_time = 0.5
 blink_on_time = 0.5
@@ -50,7 +39,7 @@ async def say_hello(request, response, name):
 
 async def send_status(request, response):
     # send boolean status and number frequency
-    response_string = json.dumps({"garage-door-closed": garage_door_closed, "status": status, "delay": (blink_off_time + blink_on_time) *0.5, "blink_on_time": blink_on_time, "blink_off_time": blink_off_time})
+    response_string = json.dumps({"door-state": magnet.value(), "status": status})
     await response.send_json(response_string, 200)
 
 async def set_blink_pattern(request, response, on, off):
@@ -109,15 +98,7 @@ async def main():
     if config.BLINK_IP:
         await(server.blink_ip(led_pin = led, last_only = config.BLINK_LAST_ONLY))
     while not shutdown:
-        if status:
-            led.on()
-            await asyncio.sleep(blink_on_time)
-            led.off()
-            await asyncio.sleep(blink_off_time)
-        else:	
-            led.off()
-            await asyncio.sleep(0.2)
-        print(magnet.value()) 
+        await asyncio.sleep(1)
             
 server = GurgleAppsWebserver(config.WIFI_SSID, config.WIFI_PASSWORD, port=80, timeout=20, doc_root="/www", log_level=2)
 server.add_function_route("/set-delay/<delay>", set_delay)
